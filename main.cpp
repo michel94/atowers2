7a0ea6c8b0e9b9f69b6efa245d149e01a70ab4c4
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-07-25 01:58:24 +0100

    Camera improvements

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "main.hpp"
 #include "loader.hpp"
 #include "shaders.hpp"
+#include "cube.hpp"
 #include <glm/glm.hpp>
 
 #define SCREEN_SIZE_CUT 1.2f
@@ -14,6 +15,9 @@ int frame = 0, window_width, window_height;
 double timebase = 0, tm = 0, last_tick;
 double fps;
 
+// Camera
+float angleX = 45, angleY = -45, posX=0, posY=-1, posZ = 4, zoom = 1;
+
 void opengl_init(){
 	glfwInit();
 	monitor_resolution(&SCREEN_WIDTH, &SCREEN_HEIGHT);
@@ -21,7 +25,7 @@ void opengl_init(){
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwSwapInterval(1);
-	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Atowers 2", NULL, NULL);
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Atowers II", NULL, NULL);
  	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
  	
 	glfwMakeContextCurrent(window);
@@ -58,42 +62,27 @@ void monitor_resolution(int *w, int *h){
 	*h = maxHeight / SCREEN_SIZE_CUT;
 }
 
-void drawSquare(bool flip){
-  float b = 0.0f, t = 1.0f;
-  if(flip)
-    swap(b, t);
-  
-  glBegin(GL_QUADS);
-    glTexCoord2f(b, b);
-    glVertex3f(0.0f, 0.0f, 0);
-    glTexCoord2f(t, b);
-    glVertex3f(1.0f, 0.0f, 0);
-    glTexCoord2f(t, t);
-    glVertex3f(1.0f, 1.0f, 0);
-    glTexCoord2f(b, t);
-    glVertex3f(0.0f, 1.0f, 0);
-  glEnd();
-  
-}
-
-void drawCube(int topTexture, int sideTexture){
-  glBindTexture(GL_TEXTURE_2D, topTexture);
-  drawSquare(false);
-  glBindTexture(GL_TEXTURE_2D, sideTexture);
-  glPushMatrix();
-    glRotatef(270, 1, 0, 0);
-    drawSquare(true);
-    glRotatef(270, 0, 1, 0);
-    drawSquare(true);
-  glPopMatrix();
-  glPushMatrix();
-    glTranslatef(1, 1, 0);
-    glRotatef(180, 0, 0, 1);
-    glRotatef(270, 1, 0, 0);
-    drawSquare(true);
-    glRotatef(270, 0, 1, 0);
-    drawSquare(true);
-  glPopMatrix();
+void update_camera(){
+  if(glfwGetKey(window, GLFW_KEY_LEFT))
+    angleX -= 1;
+  if(glfwGetKey(window, GLFW_KEY_RIGHT))
+    angleX += 1;
+  if(glfwGetKey(window, GLFW_KEY_UP) && angleY > -80)
+    angleY -= 1;
+  if(glfwGetKey(window, GLFW_KEY_DOWN) && angleY < -30)
+    angleY += 1;
+  if(glfwGetKey(window, GLFW_KEY_W))
+    posY -= 0.01;
+  if(glfwGetKey(window, GLFW_KEY_S))
+    posY += 0.01;
+  if(glfwGetKey(window, GLFW_KEY_A))
+    posX += 0.01;
+  if(glfwGetKey(window, GLFW_KEY_D))
+    posX -= 0.01;
+  if(glfwGetKey(window, GLFW_KEY_P) && zoom < 2)
+    zoom *= 1.04;
+  if(glfwGetKey(window, GLFW_KEY_O) && zoom > 0.5)
+    zoom /= 1.04;
 }
 
 int main(int argc, char **argv){
@@ -101,52 +90,37 @@ int main(int argc, char **argv){
 	
   int sideTexture = Loader::loadPng("side.png");
   int topTexture =  Loader::loadPng("top.png");
+  Cube cube(topTexture, sideTexture);
 
 	last_tick = glfwGetTime();
 
-  float x = 45, y = -45, posX=0, posY=-1;
-  vec2 dir;
+  vec3 dir;
 	
   do{
     glViewport(0, 0, window_width, window_width);
     glColor4f(1,1,1,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
     
+    update_camera();
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -10.0, 10.0);
+    glOrtho(-1.0*zoom, 1.0*zoom, -1.0*zoom, 1.0*zoom, -10.0, 10.0);
     //gluPerspective(65.0, window_width / (float) window_height, 0.01, 100.0);
-    if(glfwGetKey(window, GLFW_KEY_LEFT))
-      x -= 1;
-    if(glfwGetKey(window, GLFW_KEY_RIGHT))
-      x += 1;
-    if(glfwGetKey(window, GLFW_KEY_UP))
-      y += 1;
-    if(glfwGetKey(window, GLFW_KEY_DOWN))
-      y -= 1;
-    if(glfwGetKey(window, GLFW_KEY_W))
-      posY -= 0.01;
-    if(glfwGetKey(window, GLFW_KEY_S))
-      posY += 0.01;
-    if(glfwGetKey(window, GLFW_KEY_A))
-      posX += 0.01;
-    if(glfwGetKey(window, GLFW_KEY_D))
-      posX -= 0.01;
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glPushMatrix();
-      glTranslatef(posX, posY, 4);
-      glRotatef(y, 1, 0, 0);
-      glRotatef(x, 0, 0, 1);
+      glTranslatef(posX, posY, posZ);
+      glRotatef(angleY, 1, 0, 0);
+      glRotatef(angleX, 0, 0, 1);
       glScalef(0.1f,0.1f,0.1f);
 
       for(int i=0; i<12; i++)
         for(int j=0; j<8; j++){
           glPushMatrix();
             glTranslatef(i*1.05, j*1.05, i == 11 || j == 7 || i == 0 || j == 0 ? -1 : 0.1*(i+j)-1 );
-            drawCube(topTexture, sideTexture);
+            cube.draw();
           glPopMatrix();
         }
 
